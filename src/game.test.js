0c016bec627b3a44706e1f25241019b56c9c11b5
= <srbiotik@gmail.com>
2023-09-13 21:46:53 +0200

    Adds more tests

--- src/game.test.js
+++ src/game.test.js
@@ -1,10 +1,35 @@
+import { expect, describe, it } from '@jest/globals';
 import { Game } from './game';
 
-test('score method lowers score when no hits', () => {
-    const game = new Game({});
-    game.hits = 0;
-    game.operations = ['+', '+'];
-    game.currentLevel = 2;
-    game.score();
-    expect(game.levelOffset).toBe(-1);
+describe('score()', () => {
+    describe('when score is less than fail', () => {
+        const game = new Game({});
+        it('should levelOffset should be -1', () => {
+            game.hits = 0;
+            game.operations = ['+', '+'];
+            game.currentLevel = 2;
+            game.score();
+            expect(game.levelOffset).toBe(-1);
+        });
+    });
+    describe('when score is greater than pass', () => {
+        const game = new Game({});
+        it('should levelOffset should be 1', () => {
+            game.hits = 2;
+            game.operations = ['+', '+'];
+            game.currentLevel = 2;
+            game.score();
+            expect(game.levelOffset).toBe(1);
+        });
+    });
+    describe('when score is greater than fail but less then pass', () => {
+        const game = new Game({});
+        it('should levelOffset should be 0', () => {
+            game.hits = 3;
+            game.operations = ['+', '+', '+', '+'];
+            game.currentLevel = 2;
+            game.score();
+            expect(game.levelOffset).toBe(0);
+        });
+    });
 });
